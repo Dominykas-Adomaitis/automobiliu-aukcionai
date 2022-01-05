153fae70a3f553f9047629a7f3d6c160c8776ae0
Dominykas Adomaitis <d.adomaitis11@gmail.com>
2022-01-05 16:44:53 +0200

    Test commit

--- resources/views/admin/approval.blade.php
+++ resources/views/admin/approval.blade.php
@@ -6,7 +6,7 @@
             @if( $game->admin_approval == 0)
                 <div class="col-xs-6">
                     <div class="card" style="width: 100%;">
-                        <img class="card-img-top" src="{{ Storage::url($game->image)  }}" alt="Card image cap">
+                        <img class="card-img-top" src= "{{ Storage::url($game->image)  }}" alt="Card image cap">
                         <div class="card-block">
                             <h3 class="card-title"><a href="/games/{{ $game->id }}">{{ $game->brand }} {{ $game->model }}</a></h3>
                             <p class="small">Aukcioną paskelbė: {{ $game->user->name }}</p>
